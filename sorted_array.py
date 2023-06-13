3ddfdb037dc330c06a2bafda476be97e97f4bc1b
s24260 <s24260@pjwstk.edu.pl>
2023-06-13 20:48:33 +0200

    Odwrócenie liczenia czasów na poprawne.

--- sorted_array.py
+++ sorted_array.py
@@ -14,23 +14,23 @@ arr_sorted_1 = array_sorted.copy()
 arr_sorted_2 = array_sorted.copy()
 arr_sorted_3 = array_sorted.copy()
 
-start_time_selection = perf_counter()
-selection_sort(arr_sorted_1)
-end_time_selection = perf_counter()
-
-execution_time_selection = end_time_selection - start_time_selection
-print("Czas wykonania selection sort: ", execution_time_selection, "s")
-
 start_time_heap = perf_counter()
 heapsort(arr_sorted_2)
 end_time_heap = perf_counter()
 
-execution_time_heap = start_time_heap - end_time_heap
+execution_time_heap = end_time_heap - start_time_heap
 print("Czas wykonania heap sort: ", execution_time_heap, "s")
 
 start_time_quick = perf_counter()
 quicksort(arr_sorted_3, 0, len(arr_sorted_3) - 1)
 end_time_quick = perf_counter()
 
-execution_time_quick = start_time_quick - end_time_quick
+execution_time_quick = end_time_quick - start_time_quick
 print("Czas wykonania quick sort: ", execution_time_quick, "s")
+
+start_time_selection = perf_counter()
+selection_sort(arr_sorted_1)
+end_time_selection = perf_counter()
+
+execution_time_selection = end_time_selection - start_time_selection
+print("Czas wykonania selection sort: ", execution_time_selection, "s")
--- unsorted_array.py
+++ unsorted_array.py
@@ -10,23 +10,23 @@ array_1_unsorted = array.copy()
 array_2_unsorted = array.copy()
 array_3_unsorted = array.copy()
 
-start_time_selection = perf_counter()
-selection_sort(array_1_unsorted)
-end_time_selection = perf_counter()
-
-execution_time_bubble = end_time_selection - start_time_selection
-print("Czas wykonania selection sort: ", execution_time_bubble, "s")
-
 start_time_heap = perf_counter()
 heapsort(array_2_unsorted)
 end_time_heap = perf_counter()
 
-execution_time_heap = start_time_heap - end_time_heap
+execution_time_heap = end_time_heap - start_time_heap
 print("Czas wykonania heap sort: ", execution_time_heap, "s")
 
 start_time_quick = perf_counter()
 quicksort(array_3_unsorted, 0, len(array_3_unsorted) - 1)
 end_time_quick = perf_counter()
 
-execution_time_quick = start_time_quick - end_time_quick
+execution_time_quick = end_time_quick - start_time_quick
 print("Czas wykonania quick sort: ", execution_time_quick, "s")
+
+start_time_selection = perf_counter()
+selection_sort(array_1_unsorted)
+end_time_selection = perf_counter()
+
+execution_time_bubble = end_time_selection - start_time_selection
+print("Czas wykonania selection sort: ", execution_time_bubble, "s")
